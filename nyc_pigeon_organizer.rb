011b2f323fa663e0ed3f99c838843f0059cb7b6d
Darren L <darren.lou@gmail.com>
2019-03-16 22:22:07 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -34,7 +34,7 @@ def nyc_pigeon_organizer(data)
   
   data.each { |key, values|
     values.each {|type, pigeon_name|
-      pigeon_list[pigeon_name] => {type.to_s}
+      pigeon_list[pigeon_name] = type.to_s
       binding.pry
       }
     }
